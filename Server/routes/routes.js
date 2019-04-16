99efd08543f8da512be78d520c58ece2d2837e08
NGABONZIZA Roger Clement <roclena@gmail.com>
2019-04-16 14:07:56 +0200

    create account[finishes

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import usercontroller from "../controller/usercontroller";
+import accountcontroller from '../controller/accountcontroller';
 
 
 const router = express.Router();
@@ -7,6 +8,7 @@ const router = express.Router();
 router.get("/api/v1/users", usercontroller.getUser);
 router.post("/api/v1/auth/signup", usercontroller.createUser);
 router.post("/api/v1/auth/login", usercontroller.login);
- 
+router.get("/api/v1/accounts", accountcontroller.getAccounts);
+router.post('/api/v1/accounts/:id', accountcontroller.createAccount);
 
 export default router;
