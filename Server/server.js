ffe331846399ce0b738fcfece2f633ff51dbd06a
cleronga <43466141+cleronga@users.noreply.github.com>
2019-04-19 11:54:59 +0200

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.use(router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
